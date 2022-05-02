29ccc73878d1156113a11bf4e7f857a9bd755fd5
Aditya Priyadarshi <54215788+foxy4096@users.noreply.github.com>
2022-05-02 11:48:53 +0530

    Initial Commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+// tailwind.config.js
+module.exports = {
+  future: {},
+  content: ['index.html'],
+  theme: {
+  extend: {},
+  },
+  variants: {},
+  plugins: [
+    require('tailwindcss'),
+    require('autoprefixer'),
+  ],
+}
